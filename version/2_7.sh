dc2ca6a66c404a6bc730f2c33d8446f1fde7d52b
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-04-09 12:32:12 +0530

    Update 2_7.sh

--- version/2_7.sh
+++ version/2_7.sh
@@ -5,9 +5,9 @@ add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
 apt-get update
 sudo apt-get install -y \
   python2.7=2.7.12-1~trusty1 \
-  python-dev=2.7.12* \
+  python-dev=2.7.5-5ubuntu3 \
   python-pip=1.5.4-1ubuntu4 \
-  python-virtualenv=15.0.1+ds*
+  python-virtualenv=1.11.4-1ubuntu1
 
 # Install virtualenv
 virtualenv -p python $HOME/venv/2.7
