b5efbf58e2c5527ce27caaffec0534c9a74c24c1
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 15:19:39 +0530

    Update 3_1.sh

--- version/3_1.sh
+++ version/3_1.sh
@@ -2,8 +2,8 @@
 
 echo "================= Installing Python 3.1 ==================="
 sudo apt-get install -y \
-  python3.1=3.1.5-7+trusty1	 \
-  python3-dev=3.4.0-0ubuntu2
+  python3.1=3.1*	 \
+  python3-dev=3.4*
 
 
 echo "================= Successfully Installed Python 3.1 ==================="
