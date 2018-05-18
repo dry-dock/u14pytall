fd48da57b70249b3bb97b866f14390debfe9b9eb
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 11:11:08 +0530

    Update 3_1.sh

--- version/3_1.sh
+++ version/3_1.sh
@@ -3,7 +3,7 @@
 echo "================= Installing Python 3.1 ==================="
 sudo apt-get install -y \
   python3.1=3.1.5-7+trusty1	 \
-  python3.4-dev=3.4.0-0ubuntu2
+  python3-dev=3.4.0-0ubuntu2
 
 # Install virtualenv
 virtualenv -p python3.1 $HOME/venv/3.1
