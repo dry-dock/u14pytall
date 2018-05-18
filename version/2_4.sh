78185df0360b0d4f66c596f4ec39c03cc0a09570
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 13:36:32 +0530

    Update 2_4.sh

--- version/2_4.sh
+++ version/2_4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "================= Installing Python 2.4.6 ==================="
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt-get install -y \
   python2.4=2.4.6-8+trusty1 \
