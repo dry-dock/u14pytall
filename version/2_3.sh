0cdb54f8c4cc3df776630fafb13ca11e146d7f71
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 13:36:14 +0530

    Update 2_3.sh

--- version/2_3.sh
+++ version/2_3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "================= Installing Python 2.3.7 ==================="
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt-get install -y \
   python2.3=2.3.7-2+trusty1 \
