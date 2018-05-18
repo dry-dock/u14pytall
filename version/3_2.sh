f405107d4d718f697b9ae0577a6b771f5147e489
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 11:11:26 +0530

    Update 3_2.sh

--- version/3_2.sh
+++ version/3_2.sh
@@ -3,7 +3,7 @@
 echo "================= Installing Python 3.2 ==================="
 sudo apt-get install -y \
   python3.2=3.2.6-2+trusty1	 \
-  python3.4-dev=3.4.0-0ubuntu2
+  python3-dev=3.4.0-0ubuntu2
 
 # Install virtualenv
 virtualenv -p python3.2 $HOME/venv/3.2
