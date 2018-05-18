6179afac7fc882e42dd964384234967b8c11e725
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 15:20:12 +0530

    removing patch version

--- version/3_2.sh
+++ version/3_2.sh
@@ -2,7 +2,7 @@
 
 echo "================= Installing Python 3.2 ==================="
 sudo apt-get install -y \
-  python3.2=3.2.6-2+trusty1	 \
-  python3-dev=3.4.0-0ubuntu2
+  python3.2=3.2*	 \
+  python3-dev=3.4*
 
 echo "================= Successfully Installed Python 3.2 ==================="
