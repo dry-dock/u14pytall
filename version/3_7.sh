189bfa4d32f3d3c03f47466220f8cc57467c738c
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 16:00:07 +0530

    Update 3_7.sh

--- version/3_7.sh
+++ version/3_7.sh
@@ -3,8 +3,11 @@
 echo "================= Installing Python 3.7 ==================="
 sudo apt-get install -y \
   python3.7=3.7* \
-  python3.7-dev=3.7*
-  
+  python3.7-dev=3.7* \
+  python-pip=1.5* \
+  python-virtualenv=1.11*
+
+
   # Install virtualenv
 virtualenv -p python3.7 $HOME/venv/3.7
 
