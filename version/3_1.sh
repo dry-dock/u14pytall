2db6f36f4b15d09287237f08b04ba54a7053654f
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 15:57:11 +0530

    Update 3_1.sh

--- version/3_1.sh
+++ version/3_1.sh
@@ -3,7 +3,12 @@
 echo "================= Installing Python 3.1 ==================="
 sudo apt-get install -y \
   python3.1=3.1*	 \
-  python3-dev=3.4*
+  python3-dev=3.4* \
+  python-pip=1.5* \
+  python-virtualenv=1.11*
+
+# Install virtualenv
+virtualenv -p python $HOME/venv/3.1
 
 
 echo "================= Successfully Installed Python 3.1 ==================="
