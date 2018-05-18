546c4c9284e18136880a6eb1fa2b1f416a74f405
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 15:58:12 +0530

    Update 3_3.sh

--- version/3_3.sh
+++ version/3_3.sh
@@ -3,6 +3,11 @@
 echo "================= Installing Python 3.3 ==================="
 sudo apt-get install -y \
   python3.3=3.3*	 \
-  python3-dev=3.4*
+  python3-dev=3.4* \
+   python-pip=1.5* \
+  python-virtualenv=1.11*
+
+# Install virtualenv
+virtualenv -p python $HOME/venv/3.3
 
 echo "================= Successfully Installed Python 3.3 ==================="
