e9520f7f5dcce4988e81824a89396d6d6879f6c6
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 14:55:43 +0530

    adding env

--- version/3_7.sh
+++ version/3_7.sh
@@ -4,5 +4,8 @@ echo "================= Installing Python 3.7 ==================="
 sudo apt-get install -y \
   python3.7=3.7.0* \
   python3.7-dev=3.7.0*
+  
+  # Install virtualenv
+virtualenv -p python3.7 $HOME/venv/3.7
 
 echo "================= Successfully Installed Python 3.7 ==================="
