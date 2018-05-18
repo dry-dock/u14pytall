e6c571344d2af12f0a044957ecbcfa7674585124
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 15:18:22 +0530

    removing patch version

--- version/2_6.sh
+++ version/2_6.sh
@@ -4,10 +4,10 @@ echo "================= Installing Python 2.6.9 ==================="
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 sudo apt-get install -y \
-  python2.6=2.6.9-3.2+trusty1	 \
-  python-dev=2.7.5-5ubuntu3 \
-  python-pip=1.5.4-1ubuntu4 \
-  python-virtualenv=1.11.4-1ubuntu1
+  python2.6=2.6*	 \
+  python-dev=2.7* \
+  python-pip=1.5* \
+  python-virtualenv=1.11*
 
 # Install virtualenv
 virtualenv -p python $HOME/venv/2.6
