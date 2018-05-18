9db461468821b9103bff90186d9679b02e2659bc
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 15:22:51 +0530

    removing patch version

--- version/3_7.sh
+++ version/3_7.sh
@@ -2,8 +2,8 @@
 
 echo "================= Installing Python 3.7 ==================="
 sudo apt-get install -y \
-  python3.7=3.7.0* \
-  python3.7-dev=3.7.0*
+  python3.7=3.7* \
+  python3.7-dev=3.7*
   
   # Install virtualenv
 virtualenv -p python3.7 $HOME/venv/3.7
