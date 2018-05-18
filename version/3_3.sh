863d4962e97b9a8eb5f002dba6442cba8e8b3f0c
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-18 17:55:52 +0530

    Update 3_3.sh

--- version/3_3.sh
+++ version/3_3.sh
@@ -9,5 +9,11 @@ sudo apt-get install -y \
 
 # Install virtualenv
 virtualenv -p python $HOME/venv/3.3
+# Install pip packages
+. $HOME/venv/2.7/bin/activate
+pip install pyopenssl ndg-httpsclient pyasn1
+pip install nose mock pytest coverage
+CFLAGS="-O0" pip install lxml
+deactivate
 
 echo "================= Successfully Installed Python 3.3 ==================="
