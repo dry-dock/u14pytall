84bd4719a26a91ea31bdfe56bfad9fbb11257ef8
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-23 12:19:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ apt-get update
 pip install requests[security]
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
 
-# Installing pip-10.0.1
-wget -nv https://bootstrap.pypa.io/3.2/get-pip.py
-python get-pip.py
-python -m pip install pip==1.5.4
 pip install setuptools 
 
 # Installing virtualenv-15.1.0
