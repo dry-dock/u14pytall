d080c80bb27dbd03f6e6c1a164f569436c5b14f9
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-06-20 12:48:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 pip install requests[security]
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev libpq-dev libmysqlclient-dev
-python -m pip install --upgrade pip
 pip install setuptools 
 
 # Installing virtualenv-15.1.0
